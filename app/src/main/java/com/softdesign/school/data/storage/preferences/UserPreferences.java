f95306dabd68dbb13a72e4875f2186dc5eb60ecb
Reakleases <mrhz1t5h1y>
2016-03-02 23:05:49 +0400

    ht6 added preferences#2

--- app/src/main/java/com/softdesign/school/data/storage/preferences/UserPreferences.java
+++ app/src/main/java/com/softdesign/school/data/storage/preferences/UserPreferences.java
@@ -25,7 +25,6 @@ public class UserPreferences {
         for (int i = 0; i < userFields.size(); ++i) {
             preferenceEditor.putString(USER_FIELDS[i], userFields.get(i));
         }
-
         preferenceEditor.apply();
     }
 
